e391d5633bd53513358d0bbd76d17ef1644046bf
emrah <emrah@DESKTOP-R2KFH07>
2025-01-29 23:49:26 +0300

    -First commit

---
+++ src/test/java/deneme/İdeaSoft.java
@@ -0,0 +1,30 @@
+package deneme;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class İdeaSoft {
+
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://qatestcase.myideasoft.com");//Gidilmek istenen url e yönlendirme.
+		driver.findElement(By.xpath("/html/body/header/div/div/div/div[2]/div/form/input")).sendKeys("ürün");//Aramak istenen anaktar kelimenin search alanına yazılmazı.
+		driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/form/input")).sendKeys(Keys.ENTER);//Enter tuşuna basılması.
+		driver.findElement(By.xpath("//div[2]/div/div[1]/div/div[1]")).click();//Ürün detay sayfasına gidilmesi sağlanıyor.
+		for(int i=0;i<5;i++)
+		{
+		driver.findElement(By.xpath("/html/body/main/div/div/section/div/div[1]/div/div[2]/div/div[4]/div[2]/div/div")).click();
+		try {
+			Thread.sleep(3500);//her 3.5 saniyede bir sepete yeni ürün ekleniyor.
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
+		driver.findElement(By.xpath("/html/body/header/div/div/div/div[3]/div/div[3]/a")).click();//sepet sayfasına gidilir.
+		
+
+	}
+
+}
